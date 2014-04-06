533b7688cbc004cde27c3263ff19cd997f251bfa
Abhiram Chintangal <abhiram.chintangal@gmail.com>
2014-04-06 10:54:46 +0530

    Added vagrant and puppet code, it could use some polish.

---
+++ puppet/modules/requirements/manifests/init.pp
@@ -0,0 +1,15 @@
+class requirements {
+      exec { 'apt-get update': 
+      	   command => '/usr/bin/apt-get update',}    
+      package{ 'python-pip': 
+      	       ensure=>'present',
+	       require => Exec['apt-get update'], }
+      package{ 'puppet-pip':
+                ensure=>'present', 
+                provider => 'gem', 
+                require => Package['python-pip'],}
+      package{'django==1.4': 
+	       ensure=>'present', 
+               provider => 'pip', 
+               require => Package['puppet-pip'],}
+}
